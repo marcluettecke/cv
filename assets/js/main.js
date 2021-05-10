8e7c44498433a6039f4d5603fc884883144bba45
marc.luettecke <marc.luettecke@uni-konstanz.de>
2021-05-10 18:41:11 +0200

    finalized CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,3 +48,80 @@ function scrollActive() {
 	});
 }
 window.addEventListener('scroll', scrollActive);
+
+// SHOW SCROLL TOP
+function scrollTop() {
+	const scrollTop = document.getElementById('scroll-top');
+	// When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+	if (this.scrollY >= 200) scrollTop.classList.add('show-scroll');
+	else scrollTop.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollTop);
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun');
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+	themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+	// Add or remove the dark / icon theme
+	document.body.classList.toggle(darkTheme);
+	themeButton.classList.toggle(iconTheme);
+	// We save the theme and the current icon that the user chose
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+// reduce size and print on a4 sheet
+function scaleCv() {
+	document.body.classList.add('scale-cv');
+}
+
+// remove the size when the cv is downloaded
+function removeScaling() {
+	document.body.classList.remove('scale-cv');
+}
+
+// Generate PDF
+let areaCV = document.getElementById('area-cv');
+const options = {
+	margin: 0,
+	filename: 'MarcLuettecke_CV',
+	image: { type: 'jpeg', quality: 0.98 },
+	html2canvas: { scale: 4 },
+	jsPDF: { format: 'a4', orientation: 'portrait' }
+};
+
+// Call areaCV and HTML2PDF options
+let resumeButton = document.getElementById('resume-button');
+
+function generateReasume() {
+	html2pdf(areaCV, options);
+}
+
+resumeButton.addEventListener('click', () => {
+	scaleCv();
+
+	generateReasume();
+
+	// remove the scaling after 5 minutes
+	setTimeout(() => {
+		removeScaling();
+	}, 5000);
+});
