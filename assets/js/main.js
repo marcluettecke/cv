912a7ac63a34c4372209f9c9971ca0194d9b94fb
Marc <marc.luettecke@explicatis.com>
2021-12-02 17:42:08 +0100

    * fix styles due to icon updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,41 +59,41 @@ function scrollTop() {
 window.addEventListener('scroll', scrollTop);
 
 /*==================== DARK LIGHT THEME ====================*/
-const themeButton = document.getElementById('theme-button');
-const darkTheme = 'dark-theme';
-const iconTheme = 'bx-sun';
-
-// Previously selected topic (if user selected)
-let selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
-
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
-const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun');
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-	const profilePicture = document.getElementById('profile_img');
-	getCurrentTheme() === 'light'
-		? profilePicture.setAttribute('src', './assets/img/headshot_light.png')
-		: profilePicture.setAttribute('src', './assets/img/headshot_dark.png');
-	themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
-}
-
-// Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-	// Add or remove the dark / icon theme
-	document.body.classList.toggle(darkTheme);
-	themeButton.classList.toggle(iconTheme);
-	// We save the theme and the current icon that the user chose
-	localStorage.setItem('selected-theme', getCurrentTheme());
-	selectedTheme = localStorage.getItem('selected-theme');
-	localStorage.setItem('selected-icon', getCurrentIcon());
-	// change profile picture
-	toggleProfilePic();
-});
+// const themeButton = document.getElementById('theme-button');
+// const darkTheme = 'dark-theme';
+// const iconTheme = 'bx-sun';
+//
+// // Previously selected topic (if user selected)
+// let selectedTheme = localStorage.getItem('selected-theme');
+// const selectedIcon = localStorage.getItem('selected-icon');
+//
+// // We obtain the current theme that the interface has by validating the dark-theme class
+// const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+// const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun');
+//
+// // We validate if the user previously chose a topic
+// if (selectedTheme) {
+// 	// If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+// 	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+// 	const profilePicture = document.getElementById('profile_img');
+// 	getCurrentTheme() === 'light'
+// 		? profilePicture.setAttribute('src', './assets/img/headshot_light.png')
+// 		: profilePicture.setAttribute('src', './assets/img/headshot_dark.png');
+// 	themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+// }
+//
+// // Activate / deactivate the theme manually with the button
+// themeButton.addEventListener('click', () => {
+// 	// Add or remove the dark / icon theme
+// 	document.body.classList.toggle(darkTheme);
+// 	themeButton.classList.toggle(iconTheme);
+// 	// We save the theme and the current icon that the user chose
+// 	localStorage.setItem('selected-theme', getCurrentTheme());
+// 	selectedTheme = localStorage.getItem('selected-theme');
+// 	localStorage.setItem('selected-icon', getCurrentIcon());
+// 	// change profile picture
+// 	toggleProfilePic();
+// });
 
 // reduce size and print on a4 sheet
 function scaleCv() {
@@ -127,7 +127,7 @@ resumeButton.addEventListener('click', () => {
 
 	generateReasume();
 
-	// remove the scaling after 5 minutes
+	// remove the scaling after 5 seconds
 	setTimeout(() => {
 		removeScaling();
 	}, 5000);
